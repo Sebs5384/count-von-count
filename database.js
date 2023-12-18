81d3bda5e8f5b7c91b2a5e28a214542b60d3f2d5
Sebs5384 <operationmadjackal@hotmail.com>
2023-12-18 01:57:17 -0300

    Setup database
    This is our database setup

---
+++ database.js
@@ -0,0 +1,10 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize('database', 'user', 'password', {
+    dialect: 'sqlite',
+    host: 'localhost',
+    storage: 'database.sqlite',
+    logging: false
+})
+
+export default sequelize;
---
+++ syncdb.js
@@ -0,0 +1,3 @@
+import Users from './models/users.js'
+
+Users.sync({alter: true});

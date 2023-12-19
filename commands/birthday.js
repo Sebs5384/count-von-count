d8e5e2064f7ff2df17afad78f9739700ba446754
Sebs5384 <operationmadjackal@hotmail.com>
2023-12-19 19:14:18 -0300

    Adds a date formater
    This will pass the database a YY/MM/DD format instead of our DD/MM format that caused invalid values

--- commands/birthday.js
+++ commands/birthday.js
@@ -21,20 +21,18 @@ command.slashRun = async function slashRun(client, interaction) {
   const birthdayDate = interaction.options.getString('date');
   const birthdayUser = interaction.options.getMentionable('user');
 
-  console.log(birthdayDate);
-  console.log(birthdayUser.user.id);
-
   const isValid = isValidDateFormat(birthdayDate) === true;
+  const formatedDate = convertDateFormat(birthdayDate);
 
   if (isValid) {
     try {
       const [user, created] = await Users.findOrCreate({
         where: { user_id: birthdayUser.user.id },
-        defaults: { birthday_date: birthdayDate },
+        defaults: { birthday_date: formatedDate },
       });
 
       if (!created) {
-        await user.update({ birthday_date: birthdayDate });
+        await user.update({ birthday_date: formatedDate });
       }
 
       send(`Successfully set the birthday of ${birthdayUser} to ${birthdayDate}`);
@@ -50,6 +48,16 @@ function isValidDateFormat(date) {
   const dateRegex = /^(0[1-9]|[12][0-9]|3[01])\-(0[1-9]|1[0-2])$/;
 
   return dateRegex.test(date);
+}
+
+function convertDateFormat(date){
+  const [day, month] = date.split('-');
+  const year = new Date().getFullYear();
+  
+  const dateObject = new Date(`${year}-${month}-${day}`);
+  const formatedDate = dateObject.toISOString().split('T')[0];
+
+  return formatedDate;
 }
 
 export default command;

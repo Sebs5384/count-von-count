1b35cf74830796e042b13825f99a6bb92d99e58a
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-12-28 23:21:21 -0600

    Reformat birthdayList command
    This will format the file following our eslint rules

--- commands/birthdayList.js
+++ commands/birthdayList.js
@@ -3,35 +3,33 @@ import { getUsersTags, getUsersBirthdayDate, displayFormatedDate } from '../util
 import Users from '../models/users.js';
 
 const command = new SlashCommandBuilder()
-    .setName('birthdaylist')
-    .setDescription('Displays the list of users with their birthday')
+  .setName('birthdaylist')
+  .setDescription('Displays the list of users with their birthday');
 
-command.aliases = ['bl', 'bdaylist', 'blist']
+command.aliases = ['bl', 'bdaylist', 'blist'];
 
 command.slashRun = async function slashRun(client, interaction) {
-    const send = interaction.followUp.bind(interaction);
-    const guild = interaction.guild;
-    const users = await Users.findAll();
+  const send = interaction.followUp.bind(interaction);
+  const { guild } = interaction.guild;
+  const users = await Users.findAll();
 
-    const usersList = await getUsersTags(users, client);
-    const birthdayDateList = await getUsersBirthdayDate(users);
-    const listEmbed = createBirthdayListEmbed(client, usersList, birthdayDateList, guild);
+  const usersList = await getUsersTags(users, client);
+  const birthdayDateList = await getUsersBirthdayDate(users);
+  const listEmbed = createBirthdayListEmbed(client, usersList, birthdayDateList, guild);
 
-    send({ embeds: [listEmbed] });
-}
+  send({ embeds: [listEmbed] });
+};
 
 function createBirthdayListEmbed(client, users, dateList, guild) {
-    const birthdayUsers = users.map((user) => user);
+  const birthdayUsers = users.map((user) => user);
 
-
-    console.log(birthdayUsers)
-    return new EmbedBuilder()
-        .setTitle(`🍰 ${guild.name} Guild Upcoming Birthday List`)
-        .setThumbnail(guild.iconURL({ dynamic: true, size: 2048 }))
-        .setDescription(`Here is the list of users with their birthday \n 
-            ${users.map((user) => `**${user}** - ${displayFormatedDate(dateList[users.indexOf(user)])}`).join('\n')}`
-            )
-        .setColor(client.config.embedColor)
+  console.log(birthdayUsers);
+  return new EmbedBuilder()
+    .setTitle(`🍰 ${guild.name} Guild Upcoming Birthday List`)
+    .setThumbnail(guild.iconURL({ dynamic: true, size: 2048 }))
+    .setDescription(`Here is the list of users with their birthday \n 
+        ${users.map((user) => `**${user}** - ${displayFormatedDate(dateList[users.indexOf(user)])}`).join('\n')}`)
+    .setColor(client.config.embedColor);
 }
 
-export default command
+export default command;

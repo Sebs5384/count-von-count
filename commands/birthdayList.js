8e332b01467b563b2d2345e8ac8a102b49a144e9
Sebs5384 <operationmadjackal@hotmail.com>
2024-01-02 17:55:17 -0300

    Update birthdayList
    This will add the remaining months and days till the next birthday of the user we calculated earlier

--- commands/birthdayList.js
+++ commands/birthdayList.js
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
-import { getUsersTags, getUsersBirthdayDate, displayFormatedDate, calculateRemainingDays } from '../utils/general.js';
+import { getUsersTags, getUsersBirthdayDate, displayFormatedDate, calculateRemainingDaysTillBirthday, convertDaysToMonth } from '../utils/general.js';
 import Users from '../models/users.js';
 
 const command = new SlashCommandBuilder()
@@ -12,15 +12,18 @@ command.slashRun = async function slashRun(client, interaction) {
   const { guild } = interaction;
   const send = interaction.followUp.bind(interaction);
   const users = await Users.findAll();
+
   const usersList = await getUsersTags(client, users);
   const birthdayDateList = await getUsersBirthdayDate(users);
-  calculateRemainingDays(users);
-  const listEmbed = createBirthdayListEmbed(client, usersList, birthdayDateList, guild);
+  
+  const usersRemainingDaysTillBirthday = calculateRemainingDaysTillBirthday(users);
+  const conversionResults = convertDaysToMonth(usersRemainingDaysTillBirthday);
+  const listEmbed = createBirthdayListEmbed(client, usersList, birthdayDateList, guild, conversionResults);
 
   send({ embeds: [listEmbed] });
 };
 
-function createBirthdayListEmbed(client, users, dateList, guild) {
+function createBirthdayListEmbed(client, users, dateList, guild, results) {
   const guildName = guild.name;
   const birthdayUsers = users.map((user) => user);
   const birthdayDates = dateList.map((date) => displayFormatedDate(date));
@@ -29,7 +32,7 @@ function createBirthdayListEmbed(client, users, dateList, guild) {
     .setTitle(`🍰 ${guildName} Guild Upcoming Birthday List`)
     .setThumbnail(guild.iconURL({ dynamic: true, size: 2048 }))
     .setDescription(`Here is the list of users with their birthday \n 
-        ${birthdayUsers.map((user) => `${user} - ${birthdayDates[birthdayUsers.indexOf(user)]}`).join('\n')}`)
+        ${birthdayUsers.map((user, index) => `${index + 1}. ${user} - ${birthdayDates[index]} (In ${results[index].months} Months ${results[index].remainingDays} Days)`).join('\n')}`)
     .setColor(client.config.embedColor);
 }
 

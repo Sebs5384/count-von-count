f5ce26cdf873e19c7d3388d0d2b8499108b07301
Sebs5384 <operationmadjackal@hotmail.com>
2024-01-03 19:12:18 -0300

    Update general
    This will update the algorithm we are working on, we adding the cases where months and hours are equal to 0 and sending out the different time remaining that will be later displayed in the embed

--- utils/general.js
+++ utils/general.js
@@ -20,15 +20,16 @@ export function displayFormatedDate(date) {
   const [day, month] = date.split('-');
   const MONTHS_OF_YEAR = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jum', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
   const MONTH = Number(month) - 1;
+  const parsedDay = Number(day).toString();
 
   const dayWithSuffix = () => {
-    if (day >= 11 && day <= 13) return `${day}th`;
+    if (day >= 11 && day <= 13) return `${parsedDay}th`;
 
     switch (day % 10) {
-      case 1: return `${day}st`;
-      case 2: return `${day}nd`;
-      case 3: return `${day}rd`;
-      default: return `${day}th`;
+      case 1: return `${parsedDay}st`;
+      case 2: return `${parsedDay}nd`;
+      case 3: return `${parsedDay}rd`;
+      default: return `${parsedDay}th`;
     }
   };
 
@@ -45,7 +46,7 @@ export function getUsersBirthdayDate(users) {
   for (const date of usersBirthdayDate) {
     const parsedDate = JSON.stringify(date);
     const [day, month] = parsedDate.split('T')[0].split('-').reverse();
-    const formattedDay = `${parseInt(day, 10).toString()}`;
+    const formattedDay = Number(day).toString();
 
     const birthDate = `${formattedDay}-${month}`;
 
@@ -55,26 +56,7 @@ export function getUsersBirthdayDate(users) {
   return usersBirthdayDate;
 }
 
-export function calculateRemainingDaysTillBirthday(users) {
-  const today = new Date();
-  const currentYear = today.getFullYear();
-
-  const MILLISECONDS_IN_A_DAY = 1000 * 60 * 60 * 24;
-
-  const userRemainingDaysTillBirthday = users.map((user) => {
-    const usersBirthdayDate = new Date(user.birthday_date);
-    usersBirthdayDate.setFullYear(currentYear);
-
-    const differenceInMilliseconds = usersBirthdayDate - today;
-    const daysSinceBirthDay = Math.ceil((differenceInMilliseconds / (MILLISECONDS_IN_A_DAY)));
-    
-    return daysSinceBirthDay;
-  });
-
-  return userRemainingDaysTillBirthday;
-}
-
-export function convertDaysToMonth(days) {
+export function calculateRemainingTime(users) {
   const today = new Date();
   const currentYear = today.getFullYear();
   const MILLISECONDS_IN_DAY = 1000 * 60 * 60 * 24;
@@ -86,34 +68,49 @@ export function convertDaysToMonth(days) {
   const millisecondsInYear = FIRST_DAY_OF_NEXT_YEAR - FIRST_DAY_OF_YEAR;
   const totalDaysInYear = Math.ceil(millisecondsInYear / MILLISECONDS_IN_DAY);
 
-  totalDaysInYear % 2 === 0 ? daysInMonth[1] = 29 : daysInMonth[1] = 28;
+  totalDaysInYear % 2 === 0 ? daysInMonth[1] = 29 : null;
 
-  const results = days.map((day) => {  
-    let remainingDays = day;
-    let months = 0;
+  const remainingTime = users.map((user) => {  
+    const usersBirthdayDate = new Date(user.birthday_date);
+    usersBirthdayDate.setFullYear(currentYear);
 
-    for(let i = 0; i < daysInMonth.length; i++) {
-      const daysInCurrentMonth = daysInMonth[i];
-
-      if(remainingDays >= daysInCurrentMonth) {
-        remainingDays -= daysInCurrentMonth;
-        months++;
-      } else {
-        break;
-      }
-    }
-
-    if(months === 0) {
-      let remainingDaysInMilliseconds = remainingDays * MILLISECONDS_IN_DAY;
-      let endDate = new Date(today.getTime() + remainingDaysInMilliseconds);
-      let remainingHours = (endDate - today) / (1000 * 60 * 60) % 24;
-
-      return {remainingDays: remainingDays - 1, remainingHours}
-
-    }
-
-    return {months, remainingDays}
+    const differenceInMilliseconds = usersBirthdayDate - today;
+    const remainingDays = Math.ceil((differenceInMilliseconds / (MILLISECONDS_IN_DAY)));
+    
+    return getRemainingTime(remainingDays);
   }) 
 
-  return results;
+  return remainingTime;
+}
+
+function getRemainingTime(days) {
+  let remainingDays = days;
+  let months = 0;
+
+  for(let i = 0; i < daysInMonth.length; i++) {
+    const daysInCurrentMonth = daysInMonth[i];
+
+    if(remainingDays >= daysInCurrentMonth) {
+      remainingDays -= daysInCurrentMonth;
+      months++;
+    } else {
+      break;
+    }
+  }
+
+  if(remainingDays === 1) {
+    const currentHour = today.getHours();
+    const remainingHours = 24 - currentHour;
+
+    return remainingHours;
+  }
+
+  if(months === 0) {
+    const currentHour = today.getHours();
+    const remainingHours = 24 - currentHour;
+
+    return { remainingDays: remainingDays - 1 , remainingHours };
+  }
+
+  return {months, remainingDays}
 }

721bf8cecc598da15862a3798bae54afda969dd7
Sebs5384 <operationmadjackal@hotmail.com>
2024-01-10 00:47:32 -0300

    Setup birthday user embed
    This will display an embed with some information about the user who's having his birthday, we'll setup the logic so the bot can recognize when it is someone birthday by making it check daily

--- discord/birthdayMessage.js
+++ discord/birthdayMessage.js
@@ -1,16 +1,35 @@
 import { Events } from "discord.js";
-import { getBirthdayUser } from "../utils/general.js";
+import { getBirthdayUser, formatToDayMonth, formatDateToMonthDayWithSuffix } from "../utils/general.js";
+import { createBirthdayMessageEmbed } from "../utils/embeds.js";
 import  Users  from "../models/users.js";
 
 export const event = Events.ClientReady;
 
 export const callback = async (client, message) => {
     const users = (await Users.findAll()).map(user => user.dataValues);
-    
+
     (async () => {
         try {
             const { birthdayUser, userBirthdayDate, userGuildId } = await getBirthdayUser(users, client);
             
+            const channel = client.channels.cache.get('1184679139505078272');
+            const send = channel.send.bind(channel)
+            const guild = await channel.guild;
+            const guildMemberInfo = await guild.members.fetch(birthdayUser.id);
+            const countVonCount = await client.users.fetch('1184628941194018869');
+            const userIcon = birthdayUser.displayAvatarURL();
+            const countVonCountIcon = countVonCount.displayAvatarURL();
+            
+
+            const memberJoinDate = guildMemberInfo.joinedAt;
+            const guildName = guild.name;
+            const embedColor = client.config.embedColor;
+            const formatedDate = formatToDayMonth([memberJoinDate]);
+            const userJoinedAt = formatDateToMonthDayWithSuffix(formatedDate);
+        
+            const birthdayEmbed = createBirthdayMessageEmbed(embedColor, userIcon, birthdayUser, userJoinedAt, guildName, countVonCountIcon);
+            send({ embeds: [birthdayEmbed] });
+
         } catch (error) {
             console.log(`Error sending birthday message: ${error}`);
         }
--- utils/embeds.js
+++ utils/embeds.js
@@ -9,8 +9,17 @@ export function createBirthdayListEmbed(embedColor, guildIcon, guildName, birthd
       .setColor(embedColor);
 }
 
-export function birthdayMessageEmbed(embedColor, userIcon, userName) {
+export function createBirthdayMessageEmbed(embedColor, userIcon, userName, joinedAt, guildName, botIcon) {
     return new EmbedBuilder()
-      .setTitle(`Happy Birthday, ${userName}!`)
-      .setThumbnail(userIcon)
+      .setAuthor({ name: 'Birthday Announcement 🎉 ', iconURL: 'https://cdn-icons-png.flaticon.com/512/1553/1553725.png?ga=GA1.1.1369033001.1704853799&'})
+      .setThumbnail('https://cdn-icons-png.flaticon.com/512/5022/5022305.png?ga=GA1.1.1369033001.1704853799&')
+      .setDescription(`**Today is someone special day !!** \n Please wish ${userName} a Happy Birthday !`)
+      .addFields({ 
+        name: `\n${guildName} wishes you a splendide day !`, 
+        value: `\nThanks for being part of our community since ${joinedAt} \n We all wish you a wonderful day ! \n Cheers 🥂` 
+      },
+      { name: '\u200B', value: '\u200B' })
+      .setFooter( { text: 'For more info use /help, to see the full list of birthdays date use /birthdaylist ', iconURL: botIcon } )
+
+      .setColor(embedColor);
 }

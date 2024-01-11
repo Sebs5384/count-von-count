3c7c7a4dc2b1fc582ae7bd0739520afeb3bb9c47
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2024-01-10 21:45:39 -0600

    Changes field text
    Removing the extra exclamations

--- utils/embeds.js
+++ utils/embeds.js
@@ -1,7 +1,6 @@
 import { EmbedBuilder } from 'discord.js';
 
 export function createBirthdayListEmbed(embedColor, guildIcon, guildName, birthdayList) {
-
     return new EmbedBuilder()
       .setTitle(`🍰 ${guildName} Guild Upcoming Birthday List`)
       .setThumbnail(guildIcon)
@@ -15,8 +14,8 @@ export function createBirthdayMessageEmbed(embedColor, userIcon, userName, joine
       .setThumbnail('https://cdn-icons-png.flaticon.com/512/5022/5022305.png?ga=GA1.1.1369033001.1704853799&')
       .setDescription(`**Today is someone special day !!** \n Please wish ${userName} a Happy Birthday !`)
       .addFields({ 
-        name: `\n${guildName} wishes you a splendide day !`, 
-        value: `\nThanks for being part of our community since ${joinedAt} \n We all wish you a wonderful day ! \n Cheers 🥂` 
+        name: `\n${guildName} wishes you a splendide day `, 
+        value: `\nThanks for being part of our community since ${joinedAt} \n We all wish you a wonderful day \n Cheers 🥂` 
       },
       { name: '\u200B', value: '\u200B' })
       .setFooter( { text: 'For more info use /help, to see the full list of birthdays date use /birthdaylist ', iconURL: botIcon } )

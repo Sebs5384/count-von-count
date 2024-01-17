074b5655c2cbc633788fa9cc08c8c4bd06690b2b
Sebs5384 <operationmadjackal@hotmail.com>
2024-01-17 19:29:43 -0300

    Creates guild model

---
+++ models/guilds.js
@@ -0,0 +1,16 @@
+import { Sequelize } from "sequelize";
+import sequelize from "../database.js";
+
+const Guilds = sequelize.define('guilds', {
+    id:{
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    guild_id: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+export default Guilds;

501ecea792640468125c6c6ce5587b7a38279165
Sebs5384 <operationmadjackal@hotmail.com>
2024-01-17 19:28:53 -0300

    Update syncdb

--- syncdb.js
+++ syncdb.js
@@ -1,3 +1,7 @@
 import Users from './models/users.js'
+import Guilds from './models/guilds.js'
+import Channels from './models/channels.js'
 
 Users.sync({alter: true});
+Guilds.sync({alter: true});
+Channels.sync({alter: true});

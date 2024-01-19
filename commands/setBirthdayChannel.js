df2ee9c489f5705699681c49c54d0cc050f0aae9
Sebs5384 <operationmadjackal@hotmail.com>
2024-01-18 23:06:52 -0300

    Update setBirthdayChannel
    It adds the logic of our the command, now it will create or update depending if the channel and guild already exists

--- commands/setBirthdayChannel.js
+++ commands/setBirthdayChannel.js
@@ -1,4 +1,5 @@
 import { ChannelType, SlashCommandBuilder } from "discord.js";
+import { BirthdayChannel, Guild } from "../models/index.js";
 
 const command = new SlashCommandBuilder()
     .setName('setbirthdaychannel')
@@ -15,7 +16,37 @@ command.slashRun = async function slashRun(client, interaction) {
     const send = interaction.followUp.bind(interaction);
     const channel = interaction.options.getChannel('channel');
 
-    await send(`Successfully set the birthday channel to ${channel}`);
+
+    try {
+        const [guild, createdGuild] = await Guild.findOrCreate({
+            where: { guild_id: interaction.guild.id },
+            defaults: { guild_name: interaction.guild.name, guild_master: interaction.guild.ownerId },
+            upsert: true
+        });
+        
+        const existingChannel = await BirthdayChannel.findOne({ where: { guild_id: guild.guild_id }});
+    
+        if(existingChannel) {
+
+            await existingChannel.update({
+                birthday_channel: channel.id,
+                channel_name: channel.name,
+            });
+    
+            await send(`Successfully updated the birthday channel to ${channel}`);
+        } else {
+            const [channelData, createdChannel] = await BirthdayChannel.findOrCreate({
+                where: { birthday_channel: channel.id },
+                defaults: { channel_name: channel.name, guild_id: interaction.guild.id },     
+            });
+            await send(`Successfully set the birthday channel to ${channel}`);
+        }
+     
+    } catch (error) {
+        console.log(error);
+    }
+ 
+
 }
 
 export default command;

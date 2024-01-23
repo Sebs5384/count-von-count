b20507f539f4ab75c665efc78a04312e8c5e59f4
Sebs5384 <operationmadjackal@hotmail.com>
2024-01-23 19:58:51 -0300

    Setups help command logic
    This will display the command help and its embed

--- commands/help.js
+++ commands/help.js
@@ -1,8 +1,11 @@
 import { SlashCommandBuilder } from "discord.js";
+import { getCommandByType, getCommands } from "../utils/general.js";
+import { createHelpEmbed } from "../embeds/index.js";
+
 
 const command = new SlashCommandBuilder()
     .setName('help')
-    .setDescription('Shows all the commands and descriptions');
+    .setDescription('Shows all the commands and descriptions')
 command.aliases = ['h', 'commands', 'cmds'];
 
 command.slashRun = async function slashRun(client, interaction) {
@@ -13,10 +16,16 @@ command.slashRun = async function slashRun(client, interaction) {
 }
 
 async function runCommand(client, guild, send) {
-    console.log(client.commands.map(command => command));
+    const countVonCount = await client.users.fetch(client.config.botId);
+    const embedColor = client.config.embedColor;
+    const commands = client.commands;
+    const botIcon = countVonCount.displayAvatarURL({ dynamic: true, size: 2048 })
+    const commandByType = getCommandByType(commands);
+    const commandList = getCommands(commandByType);
+    const commandsMessage = `${Object.values(commandList).map(command => `\`${command.name}\`: \`${command.description}\``).join('\n')}`
 
 
-    send('help')
+    send({embeds: [createHelpEmbed(commandsMessage, embedColor, botIcon)]});
 }
 
 export default command;

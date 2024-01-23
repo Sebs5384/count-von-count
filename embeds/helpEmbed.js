861ff4a2828e6b0800c342066472844a83cc6a4a
Sebs5384 <operationmadjackal@hotmail.com>
2024-01-23 19:59:01 -0300

    Setup help command embed logic

--- embeds/helpEmbed.js
+++ embeds/helpEmbed.js
@@ -1,7 +1,12 @@
 import { EmbedBuilder } from "discord.js";
 
-function createHelpEmbed(embedColor) {
+function createHelpEmbed(commandsMessage, embedColor, botIcon) {
+
     return new EmbedBuilder()
+        .setAuthor({ name: `List of commands 🔍`, iconURL: botIcon })
+        .setDescription(`Here is the list of commands and descriptions`)
+        .addFields({name: 'Commands', value: `${commandsMessage}`})
+        .setFooter({ text: 'Ha ha ha, glad to help you !'})
         .setColor(embedColor)
 }
 

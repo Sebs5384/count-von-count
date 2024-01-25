487a63227c9e70830ff8ef9dc69b2bf41b1f350a
Sebs5384 <operationmadjackal@hotmail.com>
2024-01-24 22:22:57 -0300

    Update setTrackerChannel
    This will display the logic of our command, we sitll need to create the channel the perma-tracker will be located, which will consist on a channel that is blocked for everyone except the bot.

--- commands/tracker/setTrackerChannel.js
+++ commands/tracker/setTrackerChannel.js
@@ -7,6 +7,39 @@ const command = new SlashCommandBuilder()
 command.aliases = ['configtracker, ctracker'];
 
 command.slashRun = async function slashRun(client, interaction) {
+    const guild = await interaction.guild;
+    const send = interaction.followUp.bind(interaction);
+    const categoriesChannel = await guild.channels.cache.filter(channel => channel.type === ChannelType.GuildCategory);
+
+    const options = categoriesChannel.map(channel => {
+        return { label: channel.name, value: channel.id }
+    })
+
+    const row = new ActionRowBuilder()
+        .addComponents(
+            new StringSelectMenuBuilder()
+                .setCustomId('selectcategory')
+                .setPlaceholder('Nothing selected')
+                .addOptions(options.map(option => ({ label: option.label, value: option.value }))),
+        );
+
+    const message = await send({ content: 'Please the category where you would like the tracker to be created', components: [row] });
+    
+    const filter = i => i.customId === 'selectcategory' && i.user.id === interaction.user.id;
+    const collector = interaction.channel.createMessageComponentCollector({ filter, time: 50000 });
+
+    collector.on('collect', async (selectInteraction) => {
+        const selectedValue = selectInteraction.values;
+     
+        await message.edit({ content: `You have selected the category <#${selectedValue}> to create tracker channel`, components: [] });
+        collector.stop();
+    })
+
+    collector.on('end', (collected, reason) => {
+        if(reason === 'time') {
+            message.edit({ content: 'Time is up', components: [] });
+        }
+    })
 
 };
 

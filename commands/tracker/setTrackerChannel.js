a0154b9f1bde08602ebe403345f84abf14bc9ca9
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2024-01-25 20:50:22 -0600

    Cleans setTracker command

--- commands/tracker/setTrackerChannel.js
+++ commands/tracker/setTrackerChannel.js
@@ -1,4 +1,6 @@
-import { ChannelType, PermissionsBitField, SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder} from "discord.js";
+import { ChannelType, PermissionsBitField, SlashCommandBuilder} from "discord.js";
+import { getCategoryValues } from "../../utils/general.js";
+import { createSelectCategoryRow } from "../../rows/index.js";
 import { createTrackerEmbed } from "../../embeds/index.js";
 
 const command = new SlashCommandBuilder()
@@ -11,60 +13,55 @@ command.slashRun = async function slashRun(client, interaction) {
     const guild = await interaction.guild;
     const send = interaction.followUp.bind(interaction);
     const embedColor = client.config.embedColor;
-    const categoriesChannel = await guild.channels.cache.filter(channel => channel.type === ChannelType.GuildCategory);
 
-    const options = categoriesChannel.map(channel => {
-        return { label: channel.name, value: channel.id }
-    })
-
-    const row = new ActionRowBuilder()
-        .addComponents(
-            new StringSelectMenuBuilder()
-                .setCustomId('selectcategory')
-                .setPlaceholder('Nothing selected')
-                .addOptions(options.map(option => ({ label: option.label, value: option.value }))),
-        );
-
-    const message = await send({ content: 'Please the category where you would like the tracker to be created', components: [row] });
+    const guildCategory = ChannelType.GuildCategory
+    const categoryValues = await getCategoryValues(guild, guildCategory);
+    const rows = createSelectCategoryRow(categoryValues)
     
-    const filter = i => i.customId === 'selectcategory' && i.user.id === interaction.user.id;
-    const collector = interaction.channel.createMessageComponentCollector({ filter, time: 50000 });
+    const message = await send({ content: 'Please select the category where you would like the tracker to be created', components: [rows] });
+    const selectedCategoryFilter = i => i.customId === 'selectcategory' && i.user.id === interaction.user.id;
+    const ONE_MINUTE = 600000;
+
+    const collector = interaction.channel.createMessageComponentCollector({ selectedCategoryFilter, time: ONE_MINUTE });
 
     collector.on('collect', async (selectInteraction) => {
         const selectedValue = selectInteraction.values;
         const category = await guild.channels.cache.get(selectedValue[0]);
-        const bot = '1184628941194018869'
+        const bot = client.config.botId;
+        const permaTrackerChannel = await createPermaTrackerChannel(guild, category, bot);
         
-        const permaTracker = await guild.channels.create({
-            name: 'perma-tracker',
-            type: ChannelType.GuildText,
-            parent: category.id,
-            permissionOverwrites: [
-                {
-                    id: guild.id,
-                    deny: PermissionsBitField.Flags.SendMessages,
-                },
-                {
-                    id: bot,
-                    allow: PermissionsBitField.Flags.SendMessages,
-                }
-            ]
-        });
-        
-        await permaTracker.send({ embeds: [createTrackerEmbed(embedColor)] });
-
+        await permaTrackerChannel.send({ embeds: [createTrackerEmbed(embedColor)] });
         await message.edit({ content: `Perma-tracker channel has been created in the <#${selectedValue}> category`, components: [] });
-        
-
         collector.stop();
     })
 
     collector.on('end', (collected, reason) => {
         if(reason === 'time') {
-            message.edit({ content: 'Time is up', components: [] });
+            message.edit({ content: 'Time is up for selecting, try using /settrackerchannel command again.', components: [] });
         }
     })
 
 };
+
+
+async function createPermaTrackerChannel(guild, category, bot){
+    const permaTrackerChannel = await guild.channels.create({
+        name: 'perma-tracker',
+        type: ChannelType.GuildText,
+        parent: category.id,
+        permissionOverwrites: [
+            {
+                id: guild.id,
+                deny: PermissionsBitField.Flags.SendMessages,
+            },
+            {
+                id: bot,
+                allow: PermissionsBitField.Flags.SendMessages,
+            }
+        ]
+    });
+
+    return permaTrackerChannel;
+}
 
 export default command;

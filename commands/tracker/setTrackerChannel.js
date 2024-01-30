77c09b3b36db4225fbbe1fe44c1a50ad37a87f81
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2024-01-30 13:53:12 -0600

    sets the runcommand function in settrackerchannel

--- commands/tracker/setTrackerChannel.js
+++ commands/tracker/setTrackerChannel.js
@@ -13,13 +13,20 @@ command.slashRun = async function slashRun(client, interaction) {
     const guild = await interaction.guild;
     const send = interaction.followUp.bind(interaction);
     const embedColor = client.config.embedColor;
-
+    const userId = interaction.user.id;
+    const botId = client.config.botId;
     const guildCategory = ChannelType.GuildCategory;
+
+    await runCommand(guild, send, interaction,embedColor, guildCategory, userId, botId);
+};
+
+
+async function runCommand(guild, send, interaction, embedColor, guildCategory, userId, botId) {
     const categoryValues = await getCategoryValues(guild, guildCategory);
     const rows = createSelectCategoryRow(categoryValues);
     
     const message = await send({ content: 'Please select the category where you would like the tracker to be created', components: [rows] });
-    const selectedCategoryFilter = i => i.customId === 'selectcategory' && i.user.id === interaction.user.id;
+    const selectedCategoryFilter = i => i.customId === 'selectcategory' && i.user.id === userId;
     const ONE_MINUTE = 600000;
 
     const collector = interaction.channel.createMessageComponentCollector({ selectedCategoryFilter, time: ONE_MINUTE });
@@ -27,7 +34,6 @@ command.slashRun = async function slashRun(client, interaction) {
     collector.on('collect', async (selectInteraction) => {
         const selectedValue = selectInteraction.values;
         const category = await guild.channels.cache.get(selectedValue[0]);
-        const botId = client.config.botId;
         const permaTrackerChannel = await createPermaTrackerChannel(guild, category, botId);
         
         await permaTrackerChannel.send({ embeds: [createTrackerEmbed(embedColor)] });
@@ -40,9 +46,7 @@ command.slashRun = async function slashRun(client, interaction) {
             message.edit({ content: 'Time is up for selecting, try using /settrackerchannel command again.', components: [] });
         };
     });
-
-};
-
+}
 
 async function createPermaTrackerChannel(guild, category, botId){
     const permaTrackerChannel = await guild.channels.create({

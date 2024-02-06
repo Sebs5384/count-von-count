91ca39027591523ae06563863e2b90313664aae9
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2024-02-05 20:54:15 -0600

    Adds a footer to messageEmbed

--- embeds/messageEmbed.js
+++ embeds/messageEmbed.js
@@ -1,9 +1,10 @@
 import { EmbedBuilder } from "discord.js";
 
-function createMessageEmbed(message, description, embedColor, emoji = '') {
+function createMessageEmbed(message, description, embedColor, emoji = '', footer = '') {
     return new EmbedBuilder()   
         .setTitle(`${emoji}  ${message} `)
         .setDescription(description)
+        .setFooter({ text: footer })
         .setColor(embedColor);
 }
 

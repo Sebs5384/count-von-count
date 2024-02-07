3f93ecdf6024903fb65e1d72873c7b7fae4d3122
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2024-02-06 23:28:59 -0600

    Update tracker commands with its logic
    We still working on it, gonna push just so we can keep working on PC

--- commands/tracker/tracker.js
+++ commands/tracker/tracker.js
@@ -1,4 +1,6 @@
 import { SlashCommandBuilder } from "discord.js";
+import { createMessageEmbed, createTrackerEmbed } from "../../embeds/index.js";
+import { Boss, TrackerChannel } from "../../models/index.js";
 
 const command = new SlashCommandBuilder()
     .setName('tracker')
@@ -7,7 +9,35 @@ const command = new SlashCommandBuilder()
 command.aliases = ['t, tracker', 'mvps', 'bosses'];
 
 command.slashRun = async function slashRun(client, interaction ) {
-    console.log("hello mars")
+    const guild = await interaction.guild;
+    const send = interaction.followUp.bind(interaction);
+    const embedColor = client.config.embedColor;
+
+    const trackerChannelWithBosses = await TrackerChannel.findAll({
+        where: { guild_id: guild.id },
+        include: [{
+            model: Boss,
+        }]
+    });
+
+    const bosses = trackerChannelWithBosses.flatMap((trackerChannel) => trackerChannel.Bosses.map((boss) => boss.dataValues));
+    const guildBosses = [];
+    for(const boss of bosses) {
+        const mvp = {
+            name: boss.boss_name,
+            map: boss.boss_map,
+            downtime: boss.boss_downtime,
+            spawnWindow: boss.boss_spawn_window,
+            emoji: boss.boss_emoji,
+            killed: boss.updatedAt
+        }
+
+        guildBosses.push(mvp);
+    }
+
+    console.log(guildBosses);
+    await send({ embeds: [createTrackerEmbed(guildBosses, embedColor)] });
+
 }
 
 export default command;

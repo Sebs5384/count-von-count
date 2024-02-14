405fb7cbb1b3d320cd2f39f394e882505482c5dd
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2024-02-13 22:59:23 -0600

    update trackerEmbed
    This will detect if there's an mvp list currently being displayed, if there is not then it won't display any mvp on the tracker

--- src/embeds/trackerEmbed.js
+++ src/embeds/trackerEmbed.js
@@ -1,25 +1,15 @@
 import { EmbedBuilder } from 'discord.js'
 
-function createTrackerEmbed(mvpList, embedColor) {
-  const date = mvpList.map((mvp) => {
-    const newDate = new Date().toLocaleString('en-US', {
-      timeZone: 'America/Los_Angeles',
-    })
-
-    return newDate
-  })
-  console.log(date)
+function createTrackerEmbed(mvpList, embedColor) { 
+  const trackerEmbed = new EmbedBuilder()
+    .setTitle('MVP Tracker')
+    .setFooter({ text: `This tracker updates every minute, last updated at: `})
+    .setColor(embedColor);
+  if(mvpList) {
+    trackerEmbed.addFields(mvpList);
+  }
   
-  return new EmbedBuilder()
-    .setTitle(`MVP Tracker`)
-    .setFields(
-      mvpList.map((mvp) => ({
-        name: mvp.name,
-        value: `Start Time: ${mvp.killed}\n Map: ${mvp.map}`,
-      }))
-    )
-    .setFooter({ text: `This tracker updates every minute, last updated at` })
-    .setColor(embedColor)
+  return trackerEmbed
 }
 
 export default createTrackerEmbed;

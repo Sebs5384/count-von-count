1b921f90e1cd9b74aa31800639987eeb76a637d3
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2024-02-13 19:47:36 -0600

    Add track command logic
    This will track an mvp and update its killed at time accordingly to the current server time.

--- src/commands/tracker/track.js
+++ src/commands/tracker/track.js
@@ -1,13 +1,53 @@
 import { SlashCommandBuilder } from "discord.js";
+import { createMessageEmbed } from "../../embeds/index.js";
+import { TrackerChannel, Boss } from "../../models/index.js";
+import  getServerTime  from "../../api/serverTime.js";
 
 const command = new SlashCommandBuilder()
     .setName('track')
     .setDescription('Tracks an MVP')
+    .addStringOption((option) => option
+        .setName('mvp-name')
+        .setDescription('Input the name of the MVP you want to track')
+        .setRequired(true)
+    )
     .setDefaultMemberPermissions(0);
 command.aliases = ['t', 'track'];
 
 command.slashRun = async function slashRun(client, interaction) {
-    console.log("Hello jupiter")
+    const send = interaction.followUp.bind(interaction);
+    const guild = await interaction.guild;
+    const embedColor = client.config.embedColor;
+    const mvpName = interaction.options.getString('mvp-name');
+
+    await runCommand(send, guild, embedColor, mvpName)
 };
+
+async function runCommand(send, guild, embedColor, mvpName){
+    const trackerChannel = await TrackerChannel.findOne({
+        where: { guild_id: guild.id }
+    })
+
+    if(trackerChannel) {
+        const boss = await Boss.findOne({
+            where: { guild_id: guild.id, boss_name: mvpName }
+        });
+
+        if(boss) {
+            const serverTimeZone = 'America/Los_Angeles';
+            const serverTime = await getServerTime(serverTimeZone);
+
+            const updatedBoss = await boss.update({
+                boss_killed_at: serverTime.time
+            })
+
+            const trackerTitle = 'MvP Tracker';
+            const trackerMessage = `${updatedBoss.boss_name} died at ${updatedBoss.boss_killed_at}`;
+            const mvpHelpMessage = 'For more information use /mvphelp';
+            
+            send({ embeds: [createMessageEmbed(trackerTitle, trackerMessage, embedColor, '✅', mvpHelpMessage)] });
+        }
+    }
+}
 
 export default command;

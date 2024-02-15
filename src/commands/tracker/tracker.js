241366d83680654ecd5c3a70fb36530a61c36b2a
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2024-02-14 23:59:47 -0600

    Update trackers logic

--- src/commands/tracker/tracker.js
+++ src/commands/tracker/tracker.js
@@ -1,6 +1,7 @@
 import { SlashCommandBuilder } from "discord.js";
 import { createMessageEmbed, createTrackerEmbed } from "../../embeds/index.js";
 import { Boss, TrackerChannel } from "../../models/index.js";
+import { formatToClockHour } from "../../utils/general.js";
 
 const command = new SlashCommandBuilder()
     .setName('tracker')
@@ -29,15 +30,74 @@ command.slashRun = async function slashRun(client, interaction ) {
             downtime: boss.boss_downtime,
             spawnWindow: boss.boss_spawn_window,
             emoji: boss.boss_emoji,
-            killed: boss.updatedAt
+            killed: boss.boss_killed_at
         }
 
         guildBosses.push(mvp);
     }
 
-    console.log(guildBosses);
-    await send({ embeds: [createTrackerEmbed(guildBosses, embedColor)] });
+    const mvpTimers = guildBosses.map((boss) => {
+        const bossDownTime = Number(boss.downtime);
+        const bossSpawnWindow = Number(boss.spawnWindow);
+        const bossKilledAt = boss.killed;
+
+        const totalMinutesSinceKilled = getTotalMinutesSinceKilled(bossKilledAt);
+        const totalMinutesTillRange = getMinutesTillRange(bossDownTime, totalMinutesSinceKilled);
+        const nextRange = formatToClockHour(totalMinutesTillRange);
+        const bossRange = getBossRange(totalMinutesSinceKilled + 424, totalMinutesTillRange, bossSpawnWindow);
+        const bossField = getBossField(boss, bossRange);
+
+        return bossField;
+    })
+
+    await send({ embeds: [createTrackerEmbed(mvpTimers, embedColor)] });        
+
+        
 
 }
+
+function getBossField(boss, bossRange) {
+    
+    let downTimeHours = bossRange.remainingDownTimeInHours;
+    let downTimeMinutes = bossRange.remainingDownTimeInMinutes;
+    let spawnWindowHours = bossRange.remainingTimeTillSpawnInHours;
+    let spawnWindowMinutes = bossRange.remainingTimeTillSpawnInMinutes;
+
+
+}
+
+function getTotalMinutesSinceKilled(bossKilledAt) {
+    const [hours, minutes] = bossKilledAt.split(':').map((time) => Number(time));
+    const totalMinutesSinceKilled = hours * 60 + minutes
+
+    return totalMinutesSinceKilled
+}
+
+function getMinutesTillRange(bossDownTime, totalMinutesSinceKilled) {
+    const totalMinutesTillRange = totalMinutesSinceKilled + bossDownTime;
+    
+    return totalMinutesTillRange;
+}
+
+function getBossRange(totalMinutesSinceKilled, totalMinutesTillRange, bossSpawnWindow) {
+    const bossRemainingDownTime = totalMinutesTillRange - totalMinutesSinceKilled;
+    const bossRemainingTimeTillSpawn = (bossSpawnWindow + totalMinutesTillRange) - totalMinutesSinceKilled;
+
+    const remainingDownTimeInHours = Math.floor(bossRemainingDownTime / 60);
+    const remainingDownTimeInMinutes = bossRemainingDownTime % 60;
+
+    const remainingTimeTillSpawnInHours = Math.floor(bossRemainingTimeTillSpawn / 60);
+    const remainingTimeTillSpawnInMinutes = bossRemainingTimeTillSpawn % 60;
+
+    const bossRange = {
+        remainingDownTimeInHours,
+        remainingDownTimeInMinutes,
+        remainingTimeTillSpawnInHours,
+        remainingTimeTillSpawnInMinutes
+    }
+
+    return bossRange
+}
+
 
 export default command;

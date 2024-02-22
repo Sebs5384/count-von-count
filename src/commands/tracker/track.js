2802c975cce3799ff2def031c691a86e65cd448d
Sebs5384 <operationmadjackal@hotmail.com>
2024-02-22 20:26:28 -0300

    Add stimate input to track
    This will give the user the chance to stimate the death of the MVP, for instance if the user thinks the mvp died 10 mins ago he will be able to /track and give an stimate which is not required when using the command but still can be used

--- src/commands/tracker/track.js
+++ src/commands/tracker/track.js
@@ -2,6 +2,7 @@ import { SlashCommandBuilder } from "discord.js";
 import { createMessageEmbed } from "../../embeds/index.js";
 import { TrackerChannel, Boss } from "../../models/index.js";
 import { getServerTime } from "../../service/serverTime.js";
+import { getTotalMinutesFromDate } from "../../utils/general.js";
 import { operator } from "../../../database.js";
 
 const command = new SlashCommandBuilder()
@@ -12,46 +13,66 @@ const command = new SlashCommandBuilder()
         .setDescription('Input the name of the MVP you want to track')
         .setRequired(true)
     )
+    .addIntegerOption((option) => option
+        .setName('stimate')
+        .setDescription('Input the stimate of the kill in minutes')
+        .setRequired(false)
+    )
 command.aliases = ['t', 'track'];
 
 command.slashRun = async function slashRun(client, interaction) {
     const send = interaction.followUp.bind(interaction);
     const guild = await interaction.guild;
     const embedColor = client.config.embedColor;
+
+    const serverTimeZone = 'America/Los_Angeles';
+    let serverTime = await getServerTime(serverTimeZone);
+
     const mvpName = interaction.options.getString('mvp-name');
+    const mvpStimate = interaction.options.getInteger('stimate');
     const mvpHelpMessage = 'For more information use /mvphelp';
 
-    await runCommand(send, guild, embedColor, mvpName, mvpHelpMessage);
+    const boss = await Boss.findOne({
+        where: { 
+            guild_id: guild.id, 
+            boss_name: {
+                [operator.like]: mvpName
+            }
+        },
+        collate: 'NOCASE'
+    });
+
+    if(mvpStimate) {
+
+        const bossKilledAtTimestamp = new Date(boss.boss_killed_at);
+        const totalMinutesWhenKilled = getTotalMinutesFromDate(bossKilledAtTimestamp);
+        const mvpKilledAtInMilliseconds = bossKilledAtTimestamp.getTime() + (mvpStimate * 60 * 1000);
+        const updatedDateWhenKilled = new Date(mvpKilledAtInMilliseconds);
+        const updatedTime = `${updatedDateWhenKilled.getHours()}:${updatedDateWhenKilled.getMinutes().toString().padStart(2, '0')}`;
+        
+        serverTime.time = updatedTime;
+        serverTime.dateTime = updatedDateWhenKilled.toISOString();
+    }
+
+    await runCommand(send, guild, embedColor, boss, mvpName, serverTime, mvpHelpMessage);
 };
 
-async function runCommand(send, guild, embedColor, mvpName, footer){
+async function runCommand(send, guild, embedColor, boss, mvpName, serverTime, footer){
         
     const trackerChannel = await TrackerChannel.findOne({
         where: { guild_id: guild.id },
     })
 
     if(trackerChannel) {
-        const boss = await Boss.findOne({
-            where: { 
-                guild_id: guild.id, 
-                boss_name: {
-                    [operator.like]: mvpName
-                }
-            },
-            collate: 'NOCASE'
-        });
 
         if(boss) {
-            const serverTimeZone = 'America/Los_Angeles';
-            const serverTime = await getServerTime(serverTimeZone);
-
             const updatedBoss = await boss.update({
-                boss_killed_at: serverTime.time
+                boss_killed_at: serverTime.dateTime
             })
-
+    
             const trackerTitle = 'MvP Tracker';
-            const trackerMessage = `${updatedBoss.boss_name} died at ${updatedBoss.boss_killed_at}`;
-            
+            const trackerMessage = `${updatedBoss.boss_name} died at ${serverTime.time}`;
+                
             send({ embeds: [createMessageEmbed(trackerTitle, trackerMessage, embedColor, '✅', footer)] });
         } else {
             const trackerTitle = 'No MvP found';

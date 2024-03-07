7a2d1b7b5986bf3e4cce9166a376f6c75e58eba0
Sebs5384 <operationmadjackal@hotmail.com>
2024-03-07 17:48:28 -0300

    Set the relationship between boss and bossAlias

--- src/models/index.js
+++ src/models/index.js
@@ -4,6 +4,7 @@ import BirthdayChannel from "./birthdayChannel.js";
 import UserGuild from "./userGuild.js";
 import Boss from "./boss.js";
 import TrackerChannel from "./trackerChannel.js";
+import BossAlias from "./bossAlias.js";
 
 User.belongsToMany(Guild, { through: {
     model: UserGuild,
@@ -23,5 +24,8 @@ BirthdayChannel.belongsTo(Guild, { foreignKey: 'guild_id' });
 
 TrackerChannel.hasMany(Boss, { foreignKey: 'guild_id' });
 Boss.belongsTo(TrackerChannel, { foreignKey: 'guild_id' });
+
+BossAlias.belongsTo(Boss, { foreignKey: 'guild_id' });
+Boss.hasMany(BossAlias, { foreignKey: 'guild_id' });
 
 export { User, Guild, BirthdayChannel, UserGuild, Boss, TrackerChannel };

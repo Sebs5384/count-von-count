d0ba691b35996c7cad165cf4bf024f0a2d5660be
Sebs5384 <operationmadjackal@hotmail.com>
2024-03-08 22:20:04 -0300

    Add the rest of setalias command logic

--- src/commands/tracker/setAlias.js
+++ src/commands/tracker/setAlias.js
@@ -2,6 +2,7 @@ import { SlashCommandBuilder } from "discord.js";
 import { createMessageEmbed } from "../../embeds/index.js";
 import { BossAlias, TrackerChannel, Boss } from "../../models/index.js";
 import { isValidBossNameFormat } from "../../utils/general.js";
+import { operator } from "../../../database.js";
 
 const command = new SlashCommandBuilder()
     .setName('setalias')
@@ -55,7 +56,11 @@ async function runCommand(send, guild, embedColor, bossName, bossAlias) {
                 });
 
                 if(existingAlias) {
-            
+                    const bossAliasTitle = `The alias already exists`;
+                    const bossAliasMessage = `Something`
+                    const bossAliasFooter = 'Check /mvphelp for more information';
+
+                    await send({ embeds: [createMessageEmbed(bossAliasTitle, bossAliasMessage, embedColor, '❌', bossAliasFooter)] });
                 } else {
                     const existingBoss = await Boss.findOne({
                         where: {
@@ -77,27 +82,52 @@ async function runCommand(send, guild, embedColor, bossName, bossAlias) {
                             },
                             defaults: {
                                 guild_id: guild.id,
-                                boss_id: existingBoss.boss_id,
+                                boss_id: existingBoss.id,
                                 boss_alias: bossAlias
                             },
                             collate: 'NOCASE'
                         });
 
+                        const bossAliasTitle = 'Alias added successfully';
+                        const bossAliasMessage = `${bossName} has been aliased as ${bossAlias}`;
+                        const bossAliasFooter = 'Check /mvphelp for more information';
 
-                    } else {
-
+                        await send({ embeds: [createMessageEmbed(bossAliasTitle, bossAliasMessage, embedColor, '✅', bossAliasFooter)] });
                     };
                 };
-
-
             } catch (error) {
-                
+                console.error(error);
+
+                const errorTitle = 'Error while adding alias';
+                const errorMessage = `There was an error while adding alias for ${bossName}, please try again`;
+                const errorFooter = 'Check /mvphelp for more information';
+
+                await send({ embeds: [createMessageEmbed(errorTitle, errorMessage, embedColor, '❌', errorFooter)] });
             };
         } else {
+            const invalidValuesTitle = `Invalid values provided`
+            const invalidValuesMessage = `Invalid boss name and alias \n Reading: ${bossName} and ${bossAlias}`;
+            const invalidBossNameMessage = `Invalid boss name \n Reading: ${bossName}`;
+            const invalidAliasMessage = `Invalid map name \n Reading: ${bossAlias}`;
+            const invalidMessageFooterMessage = `Mvp name and alias should be alphanumeric only`;
 
-        }
+            !isValidBossName && !isValidBossAlias
+                ? send({ embeds: [createMessageEmbed(invalidValuesTitle, invalidValuesMessage, embedColor, '❌', invalidMessageFooterMessage)] })
+                : !isValidBossName
+                ? send({ embeds: [createMessageEmbed(invalidValuesTitle, invalidBossNameMessage, embedColor, '❌', invalidMessageFooterMessage)] })
+                : !isValidMapName
+                ? send({ embeds: [createMessageEmbed(invalidValuesTitle, invalidAliasMessage, embedColor, '❌', invalidMessageFooterMessage)] })
+            : null;
+        };
+
     } else {
+        const noTrackerChannelTitle = 'No tracker channel found';
+        const noTrackerMessage = 'Please configure the tracker channel first';
+        const noTrackerFooter = 'Use /settrackerchannel to create your own tracker channel';
 
+        await send({ embeds: [createMessageEmbed(noTrackerChannelTitle, noTrackerMessage, embedColor, '❌', noTrackerFooter)] });
     };
 
 };
+
+export default command;

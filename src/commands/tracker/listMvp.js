52646d140c7e3a2d7ff49f5af578762eff08bb79
Sebs5384 <operationmadjackal@hotmail.com>
2024-03-09 18:03:36 -0300

    Add bossAliases to the boss query

--- src/commands/tracker/listMvp.js
+++ src/commands/tracker/listMvp.js
@@ -1,6 +1,6 @@
 import { SlashCommandBuilder } from "discord.js";
 import { createMessageEmbed, createBossListEmbed } from "../../embeds/index.js";
-import { Boss, TrackerChannel } from "../../models/index.js";
+import { Boss, BossAlias, TrackerChannel } from "../../models/index.js";
 import { getBossValuesField, formatBossesData } from "../../utils/general.js";
 
 const command = new SlashCommandBuilder()
@@ -17,53 +17,56 @@ command.slashRun = async function slashRun(client, interaction) {
 };
 
 async function runCommand(send, guild, embedColor) {
+    const trackerChannel = await TrackerChannel.findOne({
+        where: {guild_id: guild.id}
+    });
     
-    try {
-        const trackerChannelWithBosses = await TrackerChannel.findAll({
-            where: { guild_id: guild.id },
-            include: [{ 
-                model: Boss 
-            }]
-        });
-        
-        if(!trackerChannelWithBosses) {
-            const noTrackerChannelTitle = 'No tracker channel found';
-            const noTrackerMessage = 'Please configure the tracker channel first'
-            const noTrackerFooter = 'Use /settrackerchannel to create your own tracker channel and /setmvp to add an mvp'
-
-            await send({ embeds: [createMessageEmbed(noTrackerChannelTitle, noTrackerMessage, embedColor, '❌', noTrackerFooter)] }); 
-        };
-
-        const bosses = trackerChannelWithBosses.flatMap((trackerChannel) => trackerChannel.Bosses.map((boss) => boss.dataValues));
-        const hasBosses = bosses.length > 0;
-
-        if(hasBosses) {
-            const formattedBosses = formatBossesData(bosses);
-            const bossesValuesString = formattedBosses.map((boss) => {
-                const bossValues = getBossValuesField(boss);
-                
-                return bossValues;
+    if(trackerChannel) {
+        try {
+            const bosses = await Boss.findAll({
+                where: {
+                    guild_id: guild.id
+                },
+                include: [{
+                    model: BossAlias,
+                    attributes: ['boss_alias']
+                }]
             });
-         
-            await send({ embeds: [createBossListEmbed(bossesValuesString, guild, embedColor)] });
-        } else {
-            const noBossesTitle = 'No MVPs found';
-            const noBossesMessage = 'There are no MVPs that are currently settled on your tracker';
-            const noBossesFooter = 'Check /mvphelp for more information or use /setmvp to add an mvp';
+            const hasBosses = bosses.length > 0;
 
-            await send({ embeds: [createMessageEmbed(noBossesTitle, noBossesMessage, embedColor, '❌', noBossesFooter)] });
-        }
+            if(hasBosses) {
+                const formattedBosses = formatBossesData(bosses);
+                const bossesValuesString = formattedBosses.map((boss) => {
+                    const bossValues = getBossValuesField(boss);
+                    
+                    return bossValues;
+                });
+             
+                await send({ embeds: [createBossListEmbed(bossesValuesString, guild, embedColor)] });
+            } else {
+                const noBossesTitle = 'No MVPs found';
+                const noBossesMessage = 'There are no MVPs that are currently settled on your tracker';
+                const noBossesFooter = 'Check /mvphelp for more information or use /setmvp to add an mvp';
+    
+                await send({ embeds: [createMessageEmbed(noBossesTitle, noBossesMessage, embedColor, '❌', noBossesFooter)] });
+            }
+    
+        } catch(error) {
+            console.log(`There was an error while listing the MVPs ${error}`);
+    
+            const errorTitle = 'An error occurred while fetching the list of MvPs'
+            const errorMessage = 'Please try using /listmvp again later'
+            const errorFooter = 'For more information use /mvphelp'
+            
+            await send({ embeds: [createMessageEmbed(errorTitle, errorMessage, embedColor, '❌', errorFooter)] });
+        };
+    } else {
+        const noTrackerChannelTitle = 'No tracker channel found';
+        const noTrackerMessage = 'Please configure the tracker channel first';
+        const noTrackerFooter = 'Use /settrackerchannel to create your own tracker channel';
 
-    } catch(error) {
-        console.log(error);
-
-        const errorTitle = 'An error occurred while fetching the list of MvPs'
-        const errorMessage = 'Please try using /listmvp again later'
-        const errorFooter = 'For more information use /mvphelp'
-        
-        await send({ embeds: [createMessageEmbed(errorTitle, errorMessage, embedColor, '❌', errorFooter)] });
+        await send({ embeds: [createMessageEmbed(noTrackerChannelTitle, noTrackerMessage, embedColor, '❌', noTrackerFooter)] }); 
     };
-
 };
 
 export default command

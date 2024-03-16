3fbba3d8e71e89e454d31a03fe443ae0f2733806
Sebs5384 <operationmadjackal@hotmail.com>
2024-03-15 21:12:35 -0300

    Add currentPage and totalPages parameters
    This will display the total pages and current page the pagination is on

--- src/embeds/bossListEmbed.js
+++ src/embeds/bossListEmbed.js
@@ -1,13 +1,13 @@
 import { EmbedBuilder } from "discord.js";
 
-function createBossListEmbed(bossList, bossListLength, guild, embedColor) {
+function createBossListEmbed(bossList, bossListLength, currentPage, totalPages, guild, embedColor) {
     return new EmbedBuilder()
         .setTitle(`${guild.name} MvP List`)
         .setThumbnail(guild.iconURL())
         .setDescription(`**Here's the list of all the MvPs that are currently settled**.\nCurrent amount: ${bossListLength}`)
         .addFields(bossList)
         .setColor(embedColor)
-        .setFooter({ text: 'If you wish to manage these bosses use /editmvp, /removemvp or /setmvp' });
+        .setFooter({ text: `If you wish to manage these bosses use /editmvp, /removemvp, /setalias or /setmvp.\nPage ${currentPage + 1} of ${totalPages}` });
 };
 
 export default createBossListEmbed;

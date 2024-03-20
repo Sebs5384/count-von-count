64fc47fe6ca7edeed7a9a03a98a28494893dd524
Sebs5384 <operationmadjackal@hotmail.com>
2024-03-20 01:54:15 -0300

    Update sqi embed with a new parameter
    This new parameter is being done in order to splice to position 3 and 5 if the SQI is either a weapon or armor, this position is reserved for either attackStrenght or defenseRate and has weaponLevel or not

--- src/embeds/sqiEmbed.js
+++ src/embeds/sqiEmbed.js
@@ -1,20 +1,10 @@
 import { EmbedBuilder } from "discord.js";
 
-function createSqiEmbed(sqi, embedColor) {
+function createSqiEmbed(sqi, fields, embedColor) {
     return new EmbedBuilder()
         .setAuthor({ name: sqi.name, iconURL: sqi.icon})
         .setThumbnail(sqi.image)
-        .addFields(
-            { name: 'Description', value: sqi.description },
-            { name: 'Stats', value: sqi.stats.map(stat => `${stat}`).join('\n') },
-            { name: 'Item Class', value: sqi.itemClass },
-            { name: 'Defense Rate', value: sqi.defenseRate },
-            { name: 'Weight', value: sqi.weight },
-            { name: 'Required Level', value: sqi.requiredLevel },
-            { name: 'Application Jobs', value: sqi.applicationJobs.map(job => `${job}`).join('\n') },
-            { name: 'Crafting Ingredients', value: sqi.ingredients.map(ingredient => `- ${ingredient}`).join('\n') },
-            { name: 'SQI Bonuses', value: sqi.bonuses.map(bonus => `- ${bonus}`).join('\n') }
-        )
+        .addFields(fields)
         .setFooter({ text: 'To check the full list of SQI use /listsqi'})
         .setColor(embedColor)
 };

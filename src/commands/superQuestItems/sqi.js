16f921dbdb85525ffaa51a909ac7087e24aaf7cf
Sebs5384 <operationmadjackal@hotmail.com>
2024-03-21 21:48:32 -0300

    Update SQI command logic

--- src/commands/superQuestItems/sqi.js
+++ src/commands/superQuestItems/sqi.js
@@ -1,5 +1,6 @@
 import { SlashCommandBuilder } from "discord.js";
 import { createMessageEmbed, createSqiEmbed } from "../../embeds/index.js";
+import { createSuperQuestItemButtonsRow } from "../../rows/index.js";
 import fs from "fs";
 
 const command = new SlashCommandBuilder()
@@ -18,10 +19,10 @@ command.slashRun = async function slashRun(client, interaction) {
     const embedColor = client.config.embedColor;
     const sqiName = interaction.options.getString('sqi-name');
 
-    await runCommand(send, guild, embedColor, sqiName);
+    await runCommand(send, guild, embedColor, sqiName, interaction);
 };
 
-async function runCommand(send, guild, embedColor, sqiName) {
+async function runCommand(send, guild, embedColor, sqiName, interaction) {
     try {
         const sqiFiles = fs.readdirSync('src/data/sqi');
         const matchingFileName = findMatchingFile(sqiFiles, sqiName);
@@ -30,12 +31,43 @@ async function runCommand(send, guild, embedColor, sqiName) {
             const filePath = `src/data/sqi/${matchingFileName}`;
             const sqiFile = fs.readFileSync(filePath, 'utf8');
             const sqi = JSON.parse(sqiFile);
+
             const sqiData = getSuperQuestItemData(sqi);
             const sqiFields = getSqiMainStatFields(sqiData);
             const sqiIngredients = getSqiIngredientFields(sqiData);
             const sqiBonuses = getSqiBonusFields(sqiData);
+            const sqiEmbed = createSqiEmbed(sqiData, sqiFields, embedColor);
+            const sqiEmbedButtons = createSuperQuestItemButtonsRow(sqi);
 
-            send({ embeds: [createSqiEmbed(sqiData, sqiFields, embedColor)] });
+            const message = await send({ embeds: [sqiEmbed], components: [sqiEmbedButtons] });
+            
+            try {
+                const userInteractionFilter = (i) => i.user.id === interaction.user.id;
+                const THREE_MINUTES = 180000;
+                const collector = message.createMessageComponentCollector({ filter: userInteractionFilter, time: THREE_MINUTES });
+
+                collector.on('collect', async (button) => {
+                    if(button.customId === 'sqiingredients') {
+                        const sqiEmbed = createSqiEmbed(sqiData, sqiIngredients, embedColor);
+                        const sqiEmbedButtons = createSuperQuestItemButtonsRow(sqi);
+
+                        message.edit({ embeds: [sqiEmbed], components: [sqiEmbedButtons] });
+                        await button.deferUpdate();
+                    } else if(button.customId === 'sqibonuses') {
+                        const sqiEmbed = createSqiEmbed(sqiData, sqiBonuses, embedColor);
+                        const sqiEmbedButtons = createSuperQuestItemButtonsRow(sqi);
+
+                        message.edit({ embeds: [sqiEmbed], components: [sqiEmbedButtons] });
+                        await button.deferUpdate();
+                    };
+                });
+
+
+            } catch(error) {
+                console.log(`There was an error while creating the collector: ${error}`);
+            }
+            
+            
         } else {
             const noSqiFoundTitle = 'No SQI found';
             const noSqiFoundMessage = `Please check if the name you entered is correct, reading: ${sqiName}`;

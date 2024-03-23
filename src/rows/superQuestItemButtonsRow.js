80351aa6314bd761ce34528b7bb3730729716cf1
Sebs5384 <operationmadjackal@hotmail.com>
2024-03-23 18:21:36 -0300

    Add description button to SQIbuttonsRow
    This will add a new button that will be displayed once the user interacts with either bonuses or ingredients, the logic of the function has been changed to add the corresponding components dinamically

--- src/rows/superQuestItemButtonsRow.js
+++ src/rows/superQuestItemButtonsRow.js
@@ -1,6 +1,6 @@
 import { ButtonBuilder, ActionRowBuilder, ButtonStyle } from 'discord.js';
 
-function createSuperQuestItemButtonsRow(sqi) {
+function createSuperQuestItemButtonsRow(currentButtons) {
     const ingredientsButton = new ButtonBuilder()
         .setCustomId('sqiingredients')
         .setLabel('Ingredients')
@@ -11,11 +11,24 @@ function createSuperQuestItemButtonsRow(sqi) {
         .setLabel('Bonuses')
         .setStyle(ButtonStyle.Secondary);
 
+    const descriptionButton = new ButtonBuilder()
+        .setCustomId('sqidescription')
+        .setLabel('Item Description')
+        .setStyle(ButtonStyle.Secondary);
+
+    let buttons = currentButtons.map((buttonId) => {
+        switch(buttonId) {
+            case 'sqiingredients':
+                return ingredientsButton;
+            case 'sqibonuses':
+                return bonusesButton;
+            case 'sqidescription':
+                return descriptionButton;
+        }
+    })
+    
     return new ActionRowBuilder({
-        components: [
-            ingredientsButton,
-            bonusesButton
-        ]
+        components: buttons
     });
 };
 

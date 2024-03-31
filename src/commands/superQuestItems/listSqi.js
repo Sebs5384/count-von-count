f5b70d27e72345df5169ae6345dc6fdf44328226
Sebs5384 <operationmadjackal@hotmail.com>
2024-03-30 21:44:47 -0300

    Update listSQI

--- src/commands/superQuestItems/listSqi.js
+++ src/commands/superQuestItems/listSqi.js
@@ -1,4 +1,5 @@
 import { SlashCommandBuilder } from "discord.js";
+import { createListEmbed } from "../../embeds/index.js";
 import fs from 'fs';
 
 const command = new SlashCommandBuilder()
@@ -20,7 +21,18 @@ async function runCommand(send, guild, embedColor, interaction) {
     try{
         const sqiFiles = fs.readdirSync('src/data/sqi');
         const sqiNames = getFilesName(sqiFiles, '.json');
-                
+        const sqis = sqiNames.map(sqi => {
+            return {
+                name: 'SQI LIST',
+                value: `${sqi}`
+            }
+        })
+        const sqiListTitle = 'Super Quest Items';
+        const sqiListIcon = 'https://talontales.com/wiki/images/d/d2/Mob27279.gif'
+        const sqiListDescription = 'List of all SQI items in the game';
+        const sqiListFooter = 'You can check each SQI by using /sqi <sqi name>';
+        const embed = createListEmbed(sqiListTitle, sqiListIcon, sqiListDescription, sqis, embedColor, sqiListFooter);
+        send({ embeds: [embed] });        
     } catch(error) {
         console.log(`There was an error while executing /listsqi, error: ${error}`);
     }

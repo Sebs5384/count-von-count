13cdd80d17776993f2163ab3907e2f0d7ecf1eca
Sebs5384 <operationmadjackal@hotmail.com>
2024-06-07 16:42:17 -0300

    Updates the bot to reproduce the tracker event on every guild that has it

--- src/commands/tracker/tracker.js
+++ src/commands/tracker/tracker.js
@@ -23,26 +23,28 @@ command.slashRun = async function slashRun(client, interaction, permaTrackerMess
 
     const bosses = trackerChannelWithBosses.flatMap((trackerChannel) => trackerChannel.Bosses.map((boss) => boss.dataValues));
     const hasBosses = bosses.length > 0
-    
+
     const serverTimeZone = 'America/Los_Angeles';
     const serverTime = await getServerTime(serverTimeZone);
     const trackerFooter = `Tracker updates every minute, last updated at: ${serverTime.date} ${serverTime.time} Server Time \nCurrent Location: (${serverTimeZone.replace('_', ' ')})`;
 
-    if(hasBosses) {
+    if (hasBosses) {
         const formattedBosses = formatBossesData(bosses);
         const bossTimers = getBossTimers(formattedBosses, serverTime);
-        const updatedBossTimers = await Promise.all(bossTimers.map( async (boss) => {
-            if(!boss) {
+        const updatedBossTimers = await Promise.all(bossTimers.map(async (boss) => {
+            if (!boss) {
                 return boss;
             }
 
-            if(boss.bossToRemove) {
+            if (boss.bossToRemove) {
                 await Boss.update(
                     { boss_killed_at: null },
-                    { where: {
-                        guild_id: guild.id,
-                        boss_name: boss.bossToRemove
-                    }}
+                    {
+                        where: {
+                            guild_id: guild.id,
+                            boss_name: boss.bossToRemove
+                        }
+                    }
                 );
 
                 return;
@@ -53,17 +55,9 @@ command.slashRun = async function slashRun(client, interaction, permaTrackerMess
         const filteredBossTimers = updatedBossTimers.filter(field => field)
         const hasBossesTracked = bossTimers.length > 0;
 
-        if(hasBossesTracked) {
-            if(permaTrackerMessage && permaTrackerChannelId) {
-                const externalGuildId = client.config.trackerChannelGuildInfo.id;
-                const externalChannelId = client.config.trackerChannelGuildInfo.permaTrackerChannelId;
-                const externalMessageId = client.config.trackerChannelGuildInfo.permaTrackerMessageId;
-
-                const externalGuild = await client.guilds.fetch(externalGuildId);
-                const externalTrackerChannel = await externalGuild.channels.fetch(externalChannelId);
-                const externalTrackerMessage = await externalTrackerChannel.messages.fetch(externalMessageId);
-            
-                await externalTrackerMessage.edit({ embeds: [createTrackerEmbed(filteredBossTimers, trackerFooter, embedColor)] });
+        if (hasBossesTracked) {
+            if (permaTrackerMessage && permaTrackerChannelId) {
+                
                 await permaTrackerMessage.edit({ embeds: [createTrackerEmbed(filteredBossTimers, trackerFooter, embedColor)] });
                 return;
             };
@@ -73,11 +67,11 @@ command.slashRun = async function slashRun(client, interaction, permaTrackerMess
         } else {
 
             const noBossesField = { name: 'No mvps currently tracked 🛑', value: 'Its quiet for now, go get some bosses !' }
-            if(permaTrackerMessage && permaTrackerChannelId) {
-                await permaTrackerMessage.edit({ embeds: [createTrackerEmbed(noBossesField, trackerFooter, embedColor)] });  
+            if (permaTrackerMessage && permaTrackerChannelId) {
+                await permaTrackerMessage.edit({ embeds: [createTrackerEmbed(noBossesField, trackerFooter, embedColor)] });
                 return;
             }
-            
+
             const noBosses = await send({ embeds: [createTrackerEmbed(noBossesField, trackerFooter, embedColor)] });
             return noBosses;
         }
@@ -85,8 +79,8 @@ command.slashRun = async function slashRun(client, interaction, permaTrackerMess
         const noBossesOnTrackerListTitle = 'No bosses on the tracker list';
         const noBossesOnTrackerListMessage = 'There are currently no bosses on the tracker list\nTry using /setmvp to add them into it';
         const errorTrackerFooter = 'Please try again later or use /mvphelp to get more information';
-        
-        if(permaTrackerMessage && permaTrackerChannelId) {
+
+        if (permaTrackerMessage && permaTrackerChannelId) {
             await permaTrackerMessage.edit({ embeds: [createMessageEmbed(noBossesOnTrackerListTitle, noBossesOnTrackerListMessage, embedColor, '❌', errorTrackerFooter)] });
             return;
         }
--- src/events/trackerUpdate.js
+++ src/events/trackerUpdate.js
@@ -1,27 +1,42 @@
 import { Events } from "discord.js";
+import { TrackerChannel } from "../models/index.js";
 import  callbackTracker  from "../commands/tracker/tracker.js";
 import cron from "node-cron"
 
 export const event = Events.ClientReady;
 
 export const callback = async(client) => {
-    const guildId = client.config.gonryunGuildInfo.id;
-    const channelId = client.config.gonryunGuildInfo.permaTrackerChannelId;
-    const messageId = client.config.gonryunGuildInfo.permaTrackerMessageId;
-
     const EVERY_MINUTE = '* * * * *';
 
     cron.schedule(EVERY_MINUTE, ((async () => {
         try {
-            const guild = await client.guilds.fetch(guildId);
-            const channel = await guild.channels.fetch(channelId);
-            const message = await channel.messages.fetch(messageId);
+            const trackerChannels = await TrackerChannel.findAll();
 
-            const interactionMock = {
-                guild, followUp: (content) => { message.reply(content) }
+            for(const trackerChannel of trackerChannels) {
+                const { guild_id: guildId, perma_tracker_channel_id: channelId } = trackerChannel;
+
+                try {
+                    const guild = await client.guilds.fetch(guildId);
+                    const channel = await guild.channels.fetch(channelId);
+
+                    const messages = await channel.messages.fetch({ limit: 1 });
+                    const message = messages.first();
+
+                    if(message) {
+                        const interactionMock = {
+                            guild,
+                            followUp: (content) => { message.reply(content) },
+                        };
+
+                        await callbackTracker.slashRun(client, interactionMock, message, channel);
+                    } else {
+                        console.error(`No message found in channel ${channelId}`);
+                    };
+
+                } catch(error) {
+                    console.error(`Auto updating failed in guild ${guildId}: ${error}`);
+                };
             };
-
-            await callbackTracker.slashRun(client, interactionMock, message, channel);
         } catch (error) {
             console.error(`Auto updating failed: ${error}`);
         }

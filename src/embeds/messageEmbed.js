b4218cf19d11edcae6d9a71499b3b0215a755d14
Sebs5384 <operationmadjackal@hotmail.com>
2024-08-27 23:29:24 -0300

    Changes messageEmbed to accept img footer and thumbnail

--- src/embeds/messageEmbed.js
+++ src/embeds/messageEmbed.js
@@ -1,10 +1,11 @@
 import { EmbedBuilder } from "discord.js";
 
-function createMessageEmbed(message, description, embedColor, emoji = '', footer = '') {
+function createMessageEmbed(message, description, embedColor, emoji = '', footer = null, thumbnail = null, footerImg = null) {
     return new EmbedBuilder()   
         .setTitle(`${emoji}  ${message} `)
         .setDescription(description)
-        .setFooter({ text: footer })
+        .setThumbnail(thumbnail)
+        .setFooter({ text: footer, iconURL: footerImg })
         .setColor(embedColor);
 }
 

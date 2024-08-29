2dc6c8dc74694c86f8e9cf40d400f101d31d588f
Sebs5384 <operationmadjackal@hotmail.com>
2024-08-29 08:35:41 -0300

    Add id and guild_id to race entity

--- src/models/race.js
+++ src/models/race.js
@@ -4,8 +4,21 @@ import { sequelize } from "../../database.js";
 class Race extends Model {};
 
 Race.init({
-    race_time: {
+    id: {
         type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+    },
+    next_race_time: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    race_settler_id: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    guild_id: {
+        type: Sequelize.STRING, 
         allowNull: false
     }
 },

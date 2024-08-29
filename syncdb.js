7ed11f248c389864bb67d0ad1da07518f011a074
Sebs5384 <operationmadjackal@hotmail.com>
2024-08-29 08:39:09 -0300

    Syncs RaceChannel entity

--- syncdb.js
+++ syncdb.js
@@ -1,4 +1,4 @@
-import { User, Guild, BirthdayChannel, UserGuild, Boss, TrackerChannel, BossAlias } from './src/models/index.js';
+import { User, Guild, BirthdayChannel, UserGuild, Boss, TrackerChannel, BossAlias, Race, RaceChannel } from './src/models/index.js';
 
 User.sync({alter: true});
 Guild.sync({alter: true});
@@ -6,4 +6,6 @@ BirthdayChannel.sync({alter: true});
 UserGuild.sync({alter: true});
 Boss.sync({alter: true});
 TrackerChannel.sync({alter: true});
-BossAlias.sync({alter: true})
+BossAlias.sync({alter: true});
+Race.sync({alter: true});
+RaceChannel.sync({ alter: true });

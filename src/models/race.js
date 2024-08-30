b662bac6c34295639bba3410f27737c6463ca036
Sebs5384 <operationmadjackal@hotmail.com>
2024-08-30 10:31:46 -0300

    Update race entity
    This will add the columns hours, minutes and last race timer in order to use it on the race command to later auto-updated based in these integers

--- src/models/race.js
+++ src/models/race.js
@@ -18,6 +18,18 @@ Race.init({
         type: Sequelize.STRING,
         allowNull: false
     },
+    hours_till_race: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+    },
+    minutes_till_race: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    last_settled_race_time: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
     guild_id: {
         type: Sequelize.STRING, 
         allowNull: false

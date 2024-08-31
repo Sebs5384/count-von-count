696f672c2811483f42b7c54aa958e8caedbe569e
Sebs5384 <operationmadjackal@hotmail.com>
2024-08-31 15:02:09 -0300

    Update race command with the rest of the logic
    Now it returns an embed telling the user the remaining minutes till the summer race event starts, we've also added a condition where it checks if parameters of the channels are given, in order to auto-update it later through an discord event

--- src/commands/summerRace/race.js
+++ src/commands/summerRace/race.js
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder } from "discord.js";
-import { createMessageEmbed } from "../../embeds/index.js";
+import { createMessageEmbed, createRaceEmbed } from "../../embeds/index.js";
 import { Race, RaceChannel } from "../../models/index.js";
 import { formatRaceData, getRaceTimers } from "../../utils/general.js";
 import { getServerTime } from "../../service/serverTime.js";
@@ -26,13 +26,50 @@ command.slashRun = async function slashRun(client, interaction, permaRaceMessage
 
     const serverTimeZone = 'America/Los_Angeles';
     const serverTime = await getServerTime(serverTimeZone);
+    const timerTitle = '⛱☀  Race Timer'
     const timerFooter = `Race timer updates every minute, last updated at: ${serverTime.date} ${serverTime.time} Server Time \nCurrent Location: (${serverTimeZone.replace('_', ' ')})`;
 
     if(hasTimers) {
         const formattedRaceTimers = formatRaceData(timers);
         const currentRaceTimers = getRaceTimers(formattedRaceTimers, serverTime);
 
+        const updatedRaceTimers = await Promise.all(currentRaceTimers.map(async (race) => {
+            return race; 
+        }));
         
+        const filteredRaceTimers = updatedRaceTimers.filter((race) => race !== undefined);
+        const hasRaceTimers = filteredRaceTimers.length > 0;
+
+        if(hasRaceTimers) {
+            if(permaRaceMessage && permaRaceChannelId) {
+                
+                await permaRaceMessage.edit({ embeds: [createRaceEmbed(timerTitle, filteredRaceTimers, embedColor, timerFooter)]});
+            };
+
+            const raceList = await send({ embeds: [createRaceEmbed(timerTitle, filteredRaceTimers, embedColor, timerFooter)] });
+            return raceList;
+        } else {
+            const noRacesField = { name: 'No upcoming races have been settled 🛑', value: 'To settle a race timer use /setrace'}
+            if(permaRaceMessage && permaRaceChannelId) {
+                await permaRaceMessage.edit({ embeds: [createRaceEmbed(timerTitle, noRacesField, embedColor, timerFooter)]});
+                return;
+            };
+
+            const noRaces = await send({ embeds: [createRaceEmbed(timerTitle, noRacesField, embedColor, timerFooter)] });
+            return noRaces;
+        };
+    } else {
+
+        const noRaceTrackedTitle = 'No races tracked yet';
+        const noRaceTrackedMessage = 'There are currently no race on the timer list\n Try using /setrace to add them according to the NPC timer in Port Malaya'
+        
+        if(permaRaceMessage && permaRaceChannelId) {
+            await permaRaceMessage.edit({ embeds: [createMessageEmbed(noRaceTrackedTitle, noRaceTrackedMessage, embedColor, '🛑')]});
+            return;
+        };
+
+        const noRaces = await send({ embeds: [createMessageEmbed(noRaceTrackedTitle, noRaceTrackedMessage, embedColor, '🛑')]});
+        return noRaces;
     };
 };
 

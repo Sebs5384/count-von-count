237c866479a267b308041fb24a0316d170225b67
Seb <linoshizu@gmail.com>
2025-04-09 21:58:19 -0300

    Update rosterEmbed
    We are doing this to check if there's a thumbnail to add if there's none, then we won't attach it to the embed

--- src/embeds/index.js
+++ src/embeds/index.js
@@ -1,5 +1,3 @@
-import createBirthdayListEmbed  from "./birthdayListEmbed.js";
-import createBirthdayMessageEmbed  from "./birthdayMessageEmbed.js";
 import createInfoEmbed  from "./infoEmbed.js";
 import createTrackerEmbed  from "./trackerEmbed.js";
 import createMessageEmbed  from "./messageEmbed.js";
@@ -8,16 +6,16 @@ import createSqiEmbed  from "./sqiEmbed.js";
 import createRaceEmbed from "./raceEmbed.js";
 import createNpcEmbed from "./npcEmbed.js";
 import createMonsterEmbed from "./monsterEmbed.js";
+import createRosterEmbed from "./rosterEmbed.js";
 
 export { 
     createInfoEmbed, 
-    createBirthdayListEmbed, 
-    createBirthdayMessageEmbed, 
     createTrackerEmbed, 
     createMessageEmbed, 
     createListEmbed,
     createSqiEmbed,
     createRaceEmbed,
     createNpcEmbed,
-    createMonsterEmbed
+    createMonsterEmbed,
+    createRosterEmbed
 };
--- src/embeds/rosterEmbed.js
+++ src/embeds/rosterEmbed.js
@@ -1,13 +1,17 @@
 import { EmbedBuilder } from "discord.js";
 
-function createRosterEmebed(rosterName, rosterThumbnail, organizer, mainRosterList, footer, embedColor) {
-    return new EmbedBuilder()
+function createRosterEmbed(rosterName, rosterThumbnail, organizer, mainRosterList, footer, embedColor) {
+    const embed =  new EmbedBuilder()
         .setTitle(rosterName)   
-        .setThumbnail(rosterThumbnail)
         .setAuthor(organizer)
         .setDescription(mainRosterList)
         .setFooter({ text: footer })
         .setColor(embedColor)
+    if(rosterThumbnail) {
+        embed.setThumbnail(rosterThumbnail)
+    };
+
+    return embed
 };
 
-export default createRosterEmebed;
+export default createRosterEmbed;

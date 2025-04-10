f58800abf5f3c9ea4dca9736f8c38768f1515f28
Seb <linoshizu@gmail.com>
2025-04-10 16:41:16 -0300

    Update sync file

--- syncdb.js
+++ syncdb.js
@@ -1,11 +1,13 @@
-import { User, Guild, BirthdayChannel, UserGuild, Boss, TrackerChannel, BossAlias, Race, RaceChannel } from './src/models/index.js';
+import { User, Guild, UserGuild, Boss, TrackerChannel, BossAlias, Race, RaceChannel, Roster, RosterRole } from './src/models/index.js';
+import { sequelize } from "./database.js";
 
 User.sync({alter: true});
 Guild.sync({alter: true});
-BirthdayChannel.sync({alter: true});
 UserGuild.sync({alter: true});
 Boss.sync({alter: true});
 TrackerChannel.sync({alter: true});
 BossAlias.sync({alter: true});
 Race.sync({alter: true});
 RaceChannel.sync({ alter: true });
+Roster.sync({alter: true});
+RosterRole.sync({alter: true});

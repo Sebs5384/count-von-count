a6fb8cc1ecf83b3e26dd667612f002f85c714128
5384 <operationmadjackal@hotmail.com>
2025-04-17 03:48:45 -0300

    Update interactionCreate to listen to modals
    We'll adding this in order be able to accept modals in our commands now, we are also giving the new commands the option to be defered automatically by this event file or not

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -3,6 +3,22 @@ import { Events } from 'discord.js';
 export const event = Events.InteractionCreate;
 
 export const callback = async (client, interaction) => {
+  if(interaction.isModalSubmit()) {
+    const modalId = interaction.customId;
+    const command = client.commands.find(cmd => cmd.modalSubmit && modalId.startsWith(cmd.name));
+
+    if(!command) return;
+
+    try {
+      await command.modalSubmit(client, interaction);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    };
+    
+    return;
+  };
+
   if (!interaction.isChatInputCommand()) return;
 
   const command = client.commands.get(interaction.commandName);
@@ -13,7 +29,7 @@ export const callback = async (client, interaction) => {
   }
 
   try {
-    await interaction.deferReply();
+    if(command.defer !== false) await interaction.deferReply();
     await command.slashRun(client, interaction);
   } catch (error) {
     console.error(error);

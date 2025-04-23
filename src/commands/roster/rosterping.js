feafa3be3913c00bd262bf012469888397cae89b
5384 <operationmadjackal@hotmail.com>
2025-04-22 23:41:01 -0300

    Update rosterping command
    This will update the string with ternary condtions

--- src/commands/roster/rosterping.js
+++ src/commands/roster/rosterping.js
@@ -74,7 +74,7 @@ async function runCommand(send, guild, embedColor, message, interaction) {
         await send({ embeds: [
             createMessageEmbed(
                 `${roster.roster_name}`,
-                `From <@${interaction.user.id}>: ***${message ? message : `The run is starting soon !`}***\n\n\nServer Date: ${MM_DD_YY_FORMAT}\nServer Time: ${HH_MM_FORMAT}\nThat's <t:${timeStampInSeconds}:R> for you\nRoster Note: \`${roster.roster_note}\``,
+                `From <@${interaction.user.id}>: ***${message ? message : `The run is starting soon !`}***\n\n\nServer Date: \`${MM_DD_YY_FORMAT ? MM_DD_YY_FORMAT : 'Not defined yet'}\`\nServer Time: \`${HH_MM_FORMAT ? HH_MM_FORMAT : 'Not defined yet'}\`\n${timeStampInSeconds ? `That's <t:${timeStampInSeconds}:R> for you` : ''}\nRoster Note: \`${roster.roster_note ? roster.roster_note : 'None'}\``,
                 embedColor,
                 '📢',
                 null,

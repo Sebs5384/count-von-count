98aee1956af62f5f7d09d03a61316a64355400e1
Seb <linoshizu@gmail.com>
2025-05-08 07:03:14 -0600

    Creates locate service
    This will cache the response of the users whenever the make an API call, in this way we'll prevent from making multiples calls to the same end point

---
+++ src/service/locate.js
@@ -0,0 +1,23 @@
+import { getMapLocation as getMapLocationFromApi } from "../api/locate.js";
+
+const mapLocationCache = {};
+
+export async function getMapLocation(cordinateX, cordinateY, map) {
+    const cachedKey = `${map}:${cordinateX}:${cordinateY}`;
+    if(mapLocationCache[cachedKey]) return mapLocationCache[cachedKey];
+
+    try {
+        const mapLocationFromApi = await getMapLocationFromApi(cordinateX, cordinateY, map);
+        mapLocationCache[cachedKey] = {
+            mapName: map,
+            cordinateX: cordinateX,
+            cordinateY: cordinateY,
+            url: mapLocationFromApi.url
+        };
+
+        return mapLocationFromApi;
+    } catch (error) {
+        console.error(error);
+        return null;
+    };
+};
